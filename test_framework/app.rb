d6fcb1de1285047cb5e8c76aeb82b8f48747f967
Danny Smith <hi@danny.is>
2016-10-05 17:07:25 +0100

    Reuse page objects where possible

--- test_framework/app.rb
+++ test_framework/app.rb
@@ -8,8 +8,9 @@ class App
   @@browser = Watir::Browser.new :chrome
 
   def self.method_missing(method_name, *arguments, &block)
+    @@pages ||= {}
     class_name = method_name.to_s.split('_').collect(&:capitalize).join
-    Object.const_get(class_name).new @@browser
+    @@pages[method_name] || @@pages[method_name] = Object.const_get(class_name).new(@@browser)
   end
 
   def self.close!
